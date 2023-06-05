e6e5a5f86c14afd97bdefdd09f2b4a9d0f3c97b6
hamed khodabakhsh <hamed.kh.2323@gmail.com>
2023-06-05 16:07:14 +0330

    Connecting the registration form to the api

--- components/template/SignupPage.js
+++ components/template/SignupPage.js
@@ -1,12 +1,40 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 function SignupPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const signUpHandler = async () => {
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    console.log(data);
+    if (data.status === "success") router.push("/signin");
+  };
+
   return (
     <div className="signin-form">
-      <h3>Registeration Form</h3>
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button>Register</button>
+      <h3>Registration Form</h3>
+      <input
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={signUpHandler}>Register</button>
       <div>
         <p>Have an account?</p>
         <Link href="/signin">Signin</Link>
