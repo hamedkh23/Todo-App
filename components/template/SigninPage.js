1c1cf99d32e287ac7996d3c949eef5866f991ad2
hamed khodabakhsh <hamed.kh.2323@gmail.com>
2023-06-05 18:23:45 +0330

    Connecting the login form to the api

--- components/template/SigninPage.js
+++ components/template/SigninPage.js
@@ -1,9 +1,22 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState } from "react";
+import { signIn } from "next-auth/react";
 
 function signinPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const loginHandler = async () => {
+    const res = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+    if (!res.error) router.push("/");
+  };
 
   return (
     <div className="signin-form">
