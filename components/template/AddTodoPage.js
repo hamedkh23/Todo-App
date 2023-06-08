e12d9aac4b4f7e76da2266c18f18755a160a6e4a
hamed khodabakhsh <hamed.kh.2323@gmail.com>
2023-06-08 18:12:02 +0330

    create todo page

---
+++ components/template/AddTodoPage.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import RadioButton from "../element/RadioButton";
+import { GrAddCircle } from "react-icons/gr";
+import { BsAlignStart } from "react-icons/bs";
+import { FiSettings } from "react-icons/fi";
+import { AiOutlineFileSearch } from "react-icons/ai";
+import { MdDoneAll } from "react-icons/md";
+
+function AddTodoPage() {
+  const [title, setTitle] = useState("");
+  const [status, setStatus] = useState("todo");
+
+  return (
+    <div className="add-form">
+      <h2>
+        <GrAddCircle />
+        Add New Todo
+      </h2>
+      <div className="add-form__input">
+        <div className="add-form__input--first">
+          <label htmlFor="title">Title:</label>
+          <input
+            id="title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="add-form__input--second">
+          <RadioButton
+            status={status}
+            setStatus={setStatus}
+            value="todo"
+            title="Todo"
+          >
+            <BsAlignStart />
+          </RadioButton>
+          <RadioButton
+            status={status}
+            setStatus={setStatus}
+            value="inProgress"
+            title="in Progress"
+          >
+            <FiSettings />
+          </RadioButton>
+          <RadioButton
+            status={status}
+            setStatus={setStatus}
+            value="review"
+            title="Review"
+          >
+            <AiOutlineFileSearch />
+          </RadioButton>
+          <RadioButton
+            status={status}
+            setStatus={setStatus}
+            value="done"
+            title="Done"
+          >
+            <MdDoneAll />
+          </RadioButton>
+        </div>
+        <button>Add</button>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodoPage;
