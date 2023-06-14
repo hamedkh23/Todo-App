cf43a182185901a2752d27d093f926e070e54f4f
hamed khodabakhsh <hamed.kh.2323@gmail.com>
2023-06-14 22:24:02 +0330

    Create a log out button and restrict access

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import HomePage from "@/components/template/HomePage";
+import { getSession } from "next-auth/react";
 
 function Index() {
   return <HomePage />;
 }
 
 export default Index;
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+}
